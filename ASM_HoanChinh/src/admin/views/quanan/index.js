9ffbc59ad7d2549580d729047a930d85b1228144
phuoc1238899 <145195528+phuoc1238899@users.noreply.github.com>
2024-09-13 14:46:04 +0700

    profile_quanan

--- ASM_HoanChinh/src/admin/views/quanan/index.js
+++ ASM_HoanChinh/src/admin/views/quanan/index.js
@@ -3,34 +3,36 @@ import { useNavigate } from 'react-router-dom';
 import { Card, CardContent, Box, Typography, Button} from "@mui/material";
 
 import ExQuanAn from "../../components/QuanAn/QuanAnTable";
+import { useCookies } from "react-cookie";
 
 const QuanAn = () => {
 
   const navigate = useNavigate();
+  const [cookie] = useCookies(["role"]);
 
   const handleAddQuanAn = () => {
     navigate('/admin/quanan/add');
   };
 
-    return (
-        <Box>
-      <Card variant="outlined">
-        <CardContent>
-          <Box display="flex" justifyContent="space-between" alignItems="center">
-            <Typography variant="h3" sx={{ fontWeight: "bold" }}>
-              DANH SÁCH QUÁN ĂN
-            </Typography>
-            <Button variant="contained" onClick={handleAddQuanAn}style={{width: "150px", marginRight: "40px"}}>
-              Thêm quán ăn
-            </Button>
-          </Box>
-          <Box sx={{ overflow: { xs: "auto", sm: "unset" }, fontWeight: "bold" }}>
-            <ExQuanAn/>
-          </Box>
-        </CardContent>
-      </Card>
-    </Box>
-    );
+  return (
+      <Box>
+        <Card variant="outlined">
+          <CardContent>
+            <Box display="flex" justifyContent="space-between" alignItems="center">
+              <Typography variant="h3" sx={{ fontWeight: "bold" }}>
+                {cookie.role === 0?"DANH SÁCH QUÁN ĂN":"THÔNG TIN QUÁN ĂN"}
+              </Typography>
+              <Button variant="contained" onClick={handleAddQuanAn}style={{width: "150px", marginRight: "40px"}}>
+                Thêm quán ăn
+              </Button>
+            </Box>
+            <Box sx={{ overflow: { xs: "auto", sm: "unset" }, fontWeight: "bold" }}>
+              <ExQuanAn/>
+            </Box>
+          </CardContent>
+        </Card>
+      </Box>
+  );
 };
 
 export default QuanAn;
