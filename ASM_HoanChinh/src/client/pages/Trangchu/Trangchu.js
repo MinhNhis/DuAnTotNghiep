6ca9ad6869419f8d435babdc70ac2b5679e97535
MinhNhis <145195533+MinhNhis@users.noreply.github.com>
2024-09-20 16:07:22 +0700

    RoutingMap

--- ASM_HoanChinh/src/client/pages/Trangchu/Trangchu.js
+++ ASM_HoanChinh/src/client/pages/Trangchu/Trangchu.js
@@ -13,6 +13,7 @@ import "leaflet-routing-machine";
 import geocodeAddress from '../../components/GeoLocation';
 import osm from "../../components/Map/osm-providers";
 import { makerIcon } from '../../components/Map';
+import Search from '../../components/Search/Search'
 
 
 const Trangchu = () => {
@@ -117,22 +118,7 @@ const Trangchu = () => {
     return (
         <>
             <div>
-                <div className="modal fade" id="searchModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div className="modal-dialog modal-fullscreen">
-                        <div className="modal-content rounded-0">
-                            <div className="modal-header">
-                                <h5 className="modal-title" id="exampleModalLabel">Search by keyword</h5>
-                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div className="modal-body d-flex align-items-center">
-                                <div className="input-group w-75 mx-auto d-flex">
-                                    <input type="search" className="form-control bg-transparent p-3" placeholder="keywords" aria-describedby="search-icon-1" />
-                                    <span id="search-icon-1" className="input-group-text p-3"><i className="fa fa-search"></i></span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <Search></Search>
 
                 <div className="container-fluid bg-light py-3 my-6 mt-0">
                     <div style={{ position: 'relative', width: '100%' }}>
