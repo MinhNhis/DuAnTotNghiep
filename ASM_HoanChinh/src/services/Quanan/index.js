13b59077760738b970dce20138b489e2764b639b
MinhNhis <145195533+MinhNhis@users.noreply.github.com>
2024-11-05 12:00:44 +0700

    Admin_Mo_Ta_Quan_An

--- ASM_HoanChinh/src/services/Quanan/index.js
+++ ASM_HoanChinh/src/services/Quanan/index.js
@@ -18,7 +18,7 @@ const getQuananById = async (id) => {
     return res
 }
 
-const addQuanan = async ({ ten_quan_an, hinh_anh, dia_chi, lat, lng, dien_thoai, gio_mo_cua, gio_dong_cua, link_website, link_facebook, id_gioithieu, so_luong_cho, created_user }) => {
+const addQuanan = async ({ ten_quan_an, hinh_anh, dia_chi, lat, lng, dien_thoai, gio_mo_cua, gio_dong_cua, link_website, link_facebook, id_gioithieu, so_luong_cho, mo_ta, created_user, tiennghiIds, dichvuIds, khongkhiIds, kehoachIds, baidoxeIds, loaikhIds }) => {
     const data = new FormData();
     data.append("ten_quan_an", ten_quan_an);
     data.append("dia_chi", dia_chi);
@@ -32,6 +32,43 @@ const addQuanan = async ({ ten_quan_an, hinh_anh, dia_chi, lat, lng, dien_thoai,
     data.append("id_gioithieu", id_gioithieu);
     data.append("created_user", created_user);
     data.append("so_luong_cho", so_luong_cho);
+    data.append("mo_ta", mo_ta);
+    if (tiennghiIds) {
+        tiennghiIds.forEach((e, index) => {
+            data.append(`tiennghiIds[${index}]`, e);
+        });
+    }
+
+    if (dichvuIds) {
+        dichvuIds.forEach((e, index) => {
+            data.append(`dichvuIds[${index}]`, e);
+        });
+    }
+
+    if (khongkhiIds) {
+        khongkhiIds.forEach((e, index) => {
+            data.append(`khongkhiIds[${index}]`, e);
+        });
+    }
+
+    if (kehoachIds) {
+        kehoachIds.forEach((e, index) => {
+            data.append(`kehoachIds[${index}]`, e);
+        });
+    }
+
+    if (baidoxeIds) {
+        baidoxeIds.forEach((e, index) => {
+            data.append(`baidoxeIds[${index}]`, e);
+        });
+    }
+
+    if (loaikhIds) {
+        loaikhIds.forEach((e, index) => {
+            data.append(`loaikhIds[${index}]`, e);
+        });
+    }
+
     // Gửi 1 ảnh
     data.append("hinh_anh", hinh_anh);
     const res = await request({
@@ -43,7 +80,7 @@ const addQuanan = async ({ ten_quan_an, hinh_anh, dia_chi, lat, lng, dien_thoai,
     return res
 }
 
-const editQuanan = async (id, { ten_quan_an, hinh_anh, dia_chi, lat, lng, dien_thoai, gio_mo_cua, gio_dong_cua, link_website, link_facebook, id_gioithieu, so_luong_cho, created_user, updated_user }) => {
+const editQuanan = async (id, { ten_quan_an, hinh_anh, dia_chi, lat, lng, dien_thoai, gio_mo_cua, gio_dong_cua, link_website, link_facebook, id_gioithieu, so_luong_cho, mo_ta, created_user, updated_user, tiennghiIds, dichvuIds, khongkhiIds, kehoachIds, baidoxeIds, loaikhIds }) => {
     const data = new FormData();
     data.append("ten_quan_an", ten_quan_an);
     data.append("dia_chi", dia_chi);
@@ -56,8 +93,27 @@ const editQuanan = async (id, { ten_quan_an, hinh_anh, dia_chi, lat, lng, dien_t
     data.append("link_facebook", link_facebook);
     data.append("id_gioithieu", id_gioithieu);
     data.append("so_luong_cho", so_luong_cho);
+    data.append("mo_ta", mo_ta);
     data.append("created_user", created_user);
     data.append("updated_user", updated_user);
+    tiennghiIds.forEach((e, index) => {
+        data.append(`tiennghiIds[${index}]`, e);
+    });
+    dichvuIds.forEach((e, index) => {
+        data.append(`dichvuIds[${index}]`, e);
+    });
+    khongkhiIds.forEach((e, index) => {
+        data.append(`khongkhiIds[${index}]`, e);
+    });
+    kehoachIds.forEach((e, index) => {
+        data.append(`kehoachIds[${index}]`, e);
+    });
+    baidoxeIds.forEach((e, index) => {
+        data.append(`baidoxeIds[${index}]`, e);
+    });
+    loaikhIds.forEach((e, index) => {
+        data.append(`loaikhIds[${index}]`, e);
+    });
     // Gửi 1 ảnh
     data.append("hinh_anh", hinh_anh);
     const res = await request({
@@ -69,12 +125,16 @@ const editQuanan = async (id, { ten_quan_an, hinh_anh, dia_chi, lat, lng, dien_t
     return res
 }
 
-const deleteQuanan = async (id) => {
+const deleteQuanan = async (id, { id_nguoidung, reason, role }) => {
 
     const res = await request({
         method: "DELETE",
         path: `api/quanans/${id}`,
-        data: null
+        data: {
+            reason: reason,
+            id_nguoidung: id_nguoidung,
+            role: role,
+        }
     })
 
     return res
@@ -89,7 +149,7 @@ const paginator = async (page) => {
     return res
 }
 
-const searchQuanan = async(keyword) =>{
+const searchQuanan = async (keyword) => {
     const res = await request({
         method: 'GET',
         path: `/api/search-quanan?keyword=${keyword}`
@@ -98,4 +158,4 @@ const searchQuanan = async(keyword) =>{
     return res
 }
 
-export {paginator, getQuanan, getQuananById, addQuanan, editQuanan, deleteQuanan, searchQuanan}
+export { paginator, getQuanan, getQuananById, addQuanan, editQuanan, deleteQuanan, searchQuanan }
